7f6b78d630ffc7601d3cf8d12ded3d0345d2b660
hasbihz24 <hasbihz24@gmail.com>
2024-06-10 10:21:19 +0700

    Add Introduction

--- HiBund-app/src/components/beranda/jumbotron-left.tsx
+++ HiBund-app/src/components/beranda/jumbotron-left.tsx
@@ -5,7 +5,7 @@ function JumbotronLeft(){
         <h4 className="font-jumbotron font-pink mt-n3">Generasi Hebat Bangsa!</h4>
         <p className="font-black fs-6">Temukan tips, panduan, dan komunitas yang mendukung perjalanan parenting Anda.</p>
         <form className="d-flex">
-          <button className="btn btn-pink pt-2 pb-2 px-5 text-uppercase font-coba" type="submit">Coba Sekarang GRATIS</button> <img className="ms-4" src="star.svg" />
+          <button className="btn btn-pink pt-2 pb-2 px-5 text-uppercase font-coba" type="submit">Coba Sekarang GRATIS</button>
         </form>
         </div>
     )
