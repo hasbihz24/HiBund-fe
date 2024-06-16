04d65545cf9d8e1fd68606b5ae6fca36f2bc7b16
nayasuna <nayasuna01@students.amikom.ac.id>
2024-06-16 22:42:49 +0700

    membuat register

--- HiBund-app/src/App.jsx
+++ HiBund-app/src/App.jsx
@@ -1,12 +1,13 @@
 import Login from "./pages/login";
 import ChangePassword from "./pages/ubah-pw";
+import Register from "./pages/regis";
 
 
 
 function App() {
   return (
     <>
-      <Login></Login>
+    <Register></Register>
     </>
   );
 }
