e15e3c92dbff2157d61dc5ecc35b6bf35a92ede8
nayasuna <nayasuna01@students.amikom.ac.id>
2024-06-18 19:26:09 +0700

    menambahkan navbar dan footer

--- HiBund-app/src/App.jsx
+++ HiBund-app/src/App.jsx
@@ -5,20 +5,16 @@ import ChangePassword from "./pages/ubah-pw";
 import Verify from "./Pages/verifikasi-otp";
 import VerifyLanjut from "./pages/verify2";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/navbar";
+import Footer from "./components/footer";
 
 
 
 function App() {
   return (
     <>
-        <Routes>
-          <Route path="/" element={<Login />}></Route>
-          <Route path="/regis" element={<Register/>} ></Route>
-          <Route path="/forget" element={<Forget/>}></Route>
-          <Route path="/ubah" element={<ChangePassword/>}></Route>
-          <Route path="/otp" element={<Verify/>}></Route>
-          <Route path="/otp2" element={<VerifyLanjut/>}></Route>
-        </Routes>
+       <NavBar></NavBar>
+       <Footer></Footer>
     </>
   );
 }
