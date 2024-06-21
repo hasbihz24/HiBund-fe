d695d5e72a84079a92a581ca6807ceaa165616e0
nayasuna <nayasuna01@students.amikom.ac.id>
2024-06-21 12:50:11 +0700

    benerin ubah password

--- HiBund-app/src/Pages/ubah-pw.jsx
+++ HiBund-app/src/Pages/ubah-pw.jsx
@@ -4,12 +4,17 @@ import BackButton from '../components/back-button';
 
 const ChangePassword = () => {
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const [passwordValidations, setPasswordValidations] = useState({
     length: false,
     uppercase: false,
     number: false,
     specialChar: false
   });
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handlePasswordChange = (e) => {
     const value = e.target.value;
@@ -36,22 +41,20 @@ const ChangePassword = () => {
           <img className="rounded-2xl max-w-full h-auto md:max-w-1/2" src={imglogin} alt="Login visual" />
         </div>
 
-        {/* Bagian form */}
+        {/* Form Section */}
         <div className="w-full md:w-1/2 px-4 py-8 md:px-12 md:py-16 flex flex-col gap-4 md:gap-8 bg-white">
-          {/* Navigasi Kembali dan Status Langkah */}
+          {/* Back Navigation and Step Status */}
           <div className="flex justify-between items-center">
             <div>
-              {/* Simbol panah kiri untuk kembali */}
               <BackButton nama="Kembali" />
             </div>
-
             <div className="text-right text-xs">
               <div className="text-zinc-800">Step 3 of 3</div>
               <div className="text-pink-500 font-medium">Lupa Kata Sandi</div>
             </div>
           </div>
 
-          {/* Judul dan Deskripsi */}
+          {/* Title and Description */}
           <div className="mt-4 md:mt-8">
             <h2 className="text-black text-2xl lg:text-[32px] font-bold">Buat Kata Sandi Baru</h2>
             <p className="text-stone-400 text-xs leading-normal mt-2">
@@ -71,7 +74,8 @@ const ChangePassword = () => {
                 name="email"
                 autoComplete="off"
                 placeholder="Masukkan email anda"
-                required />
+                required
+              />
             </div>
 
             {/* Password Input */}
@@ -80,28 +84,53 @@ const ChangePassword = () => {
               <div className="relative">
                 <input
                   className="px-4 py-3 lg:py-4 placeholder-gray-400 bg-white rounded-xl border border-black text-sm shadow focus:outline-none focus:ring focus:ring-black-200 focus:shadow-outline w-full focus:z-10 sm:text-sm"
-                  type="password"
+                  type={showPassword ? 'text' : 'password'}
                   id="password"
                   name="password"
                   placeholder="Masukkan kata sandi anda"
                   onChange={handlePasswordChange}
                   value={password}
-                  required />
-                {/* SVG Eye Icon */}
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer"
-                  viewBox="0 0 16 16" onClick={() => console.log('Toggle password visibility')}>
-                  <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-                  <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                  required
+                />
+                {/* SVG Icons for Eye and Eye Slash */}
+                <div onClick={toggleShowPassword} className="absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer">
+                  {showPassword ? (
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-slash" viewBox="0 0 16 16">
+                  <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z"/>
+                  <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829"/>
+                  <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z"/>
                 </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="gray"
+                      className="bi bi-eye"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                      <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                    </svg>
+                  )}
+                </div>
               </div>
             </div>
 
             {/* Password Requirements */}
-            <div className="flex flex-col gap-1 text-xs text-slate-400">
-              <div>{passwordValidations.length ? '✓' : '◻️'} Password must be between 8 to 32 characters.</div>
-              <div>{passwordValidations.uppercase ? '✓' : '◻️'} Must contain an uppercase character.</div>
-              <div>{passwordValidations.number ? '✓' : '◻️'} Must contain a number.</div>
-              <div>{passwordValidations.specialChar ? '✓' : '◻️'} Must contain one special character.</div>
+            <div className="flex flex-col gap-1 text-xs">
+              <div className={passwordValidations.length ? 'text-pink-500' : 'text-slate-400'}>
+                {passwordValidations.length ? '✓' : '◻️'} Password must be between 8 to 32 characters.
+              </div>
+              <div className={passwordValidations.uppercase ? 'text-pink-500' : 'text-slate-400'}>
+                {passwordValidations.uppercase ? '✓' : '◻️'} Must contain an uppercase character.
+              </div>
+              <div className={passwordValidations.number ? 'text-pink-500' : 'text-slate-400'}>
+                {passwordValidations.number ? '✓' : '◻️'} Must contain a number.
+              </div>
+              <div className={passwordValidations.specialChar ? 'text-pink-500' : 'text-slate-400'}>
+                {passwordValidations.specialChar ? '✓' : '◻️'} Must contain one special character.
+              </div>
             </div>
 
             {/* Submit Button */}
