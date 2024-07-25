223ae5a49372cf15b512c8defea5487689d63901
hasbihz24 <hasbihz24@gmail.com>
2024-07-25 19:21:04 +0700

    Deploy

--- HiBund-app/vite.config.js
+++ HiBund-app/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/HiBund-fe/",
   plugins: [react()],
-})
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+});
